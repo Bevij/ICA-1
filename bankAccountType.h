7a9eab1408a58c92323204654f56514e79e04110
zwiesman <zwiesman@gmail.com>
2024-09-05 19:08:42 -0700

    Added bankAccountType

---
+++ bankAccountType.h
@@ -0,0 +1,40 @@
+#ifndef bankAccountType_H
+#define bankAccountType_H
+
+#include <string>
+using namespace std;
+
+class bankAccountType
+{
+	public:
+
+		// Constructor
+
+		bankAccountType(string n = "", int acctNumber = 0, double bal = 0);
+
+		// Getters
+
+		int getAccountNumber();
+		double getBalance();
+		string getName();
+
+		// Setters
+
+		void setName(string n);
+
+		// Functions
+
+		virtual void withdraw(double amount);
+		void deposit(double amount);
+		virtual void createMonthlyStatement() = 0;
+		virtual void print();
+
+	protected:
+
+		int accountNumber;
+		string name;
+		double balance;
+};
+
+
+#endif
