e6f8d7292264144ecd2695fb5287bb1a8b4e1df3
zwiesman <zwiesman@gmail.com>
2024-09-12 14:54:47 -0700

    Fixed potential bug with randomnum

--- randomnum.cpp
+++ randomnum.cpp
@@ -4,22 +4,23 @@
 
 int randomNum(){
 
+	int cycles = 0;
 	int randNum = 0;
-	time_t seconds = time(NULL);					// Seed for random numbers, can be used to determine time out in lock function
-	bool exit = false;
 	//int numAccountsDifferent;
-
-	srand((unsigned) seconds);						// Setting the random number seed
+	time_t seconds = time(NULL);							// Seed for random numbers, can be used to determine time out in lock function
+	bool exit = false;
 
 	do{
-		randNum = (10000 + rand() % 90000);		// Sets range for random number from 10000 - 99999
-															// Starts at 10000 in order for the number to always be 5 digits
+
+		srand((unsigned) seconds * (cycles + 27));	// Setting the random number seed
+		randNum = (10000 + rand() % 90000);				// Sets range for random number from 10000 - 99999
+																	// Starts at 10000 in order for the number to always be 5 digits
 
 		// Waiting for database, but will just return a random 5 digit number for now
 		/*
-		numAccountsDifferent = 0;					// Checks to see if all the accounts have a different number than generated
+		numAccountsDifferent = 0;							// Checks to see if all the accounts have a different number than generated
 
-		// loops through to check if any account number match and counts them, will exit if any do
+		// Loops through to check if any account number match and counts them, will exit loop if any numbers match
 
 		for(int i = 0; i < numAccounts; i++)
 		{
@@ -33,14 +34,16 @@ int randomNum(){
 			}
 		}
 
-		// final check to see if all the accounts are different
+		// Exit check to see if random number is different than all accounts numbers
 
 		if(numAccountsDifferent == numAccounts)
 		{
 			exit = true;
 		}
 		*/
+		cycles++;												// Ensures that the seed will get randomized again while looping
 		exit = true;
+
 	}while(!exit);
 
 	return randNum;
