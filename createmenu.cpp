ade82716193177abd564f0138ca45e401066b5a5
zwiesman <zwiesman@gmail.com>
2024-09-18 01:06:53 -0700

    Fixed menus in edit menu

--- createmenu.cpp
+++ createmenu.cpp
@@ -3,69 +3,58 @@
 void createMenu()
 {
 	char choice;
+	char accountChar;
+	string name;
+	int accountNum;
+	double balance;
+	int numChecksWritten;
 
-   bool exit = true;
+   bool leaving;
 
-	while(exit == true)
+	while(!leaving)
 	{
 		cout << "\033c";
 		cout << "\033[2J\033[1;1H" << endl;
-		cout << "What Type of Account would you like to make." << endl;
-		cout << "1.Bank Account" << endl << "------------" << endl;
-		cout << "2.Savings Account" << endl;
-		cout << "3.High Interest Savings" << endl << "------------" << endl;
-		cout << "4.Checking Account" << endl;
-		cout << "5.High Interest Checking" << endl;
-		cout << "6.No Service Charge Checking" << endl << "------------" << endl;
-		cout << "7.Certificate of Deposit" << endl << "------------" << endl;
-		cout << "8.Exit" << endl;
+		cout << "What Type of Account would you like to make?" << endl;
+		cout << "1.Service Charge Checking Account" << endl;
+		cout << "2.No Service Charge Checking" << endl;
+		cout << "3.High Interest Checking" << endl;
+		cout << "4.Certificate of Deposit" << endl;
+		cout << "5.Savings Account" << endl;
+		cout << "6.High Interest Savings" << endl;
+		cout << "0.Exit" << endl;
 		cout << "Please enter a choice" << endl;
+
 		cin >> choice;
 		switch(choice)
 		{
-			case '1':
-				cout<<"Creating Bank Account"<<endl;
-				cin.ignore();
-				cin.ignore();
-			break;
+			case '1': // Service Charge Checking
+				accountChar = 'y';
+				accountNum = randomNum(); // no checks yet...
+
+				ofstream fileout(accountNum);// fileout is cout, but to file
+
+				cout << "\033c";
+				cout << "Name of Account Holder: "
+				getline(cin, name);
+				cout << "Initial Deposit: "
+				cin >> balance;
+				numChecksWritten = 0; // it's a new account, after all
+
+				fileout << accountChar << endl;
+				fileout << name << endl;
+				fileout << balance << endl;
+				fileout << numChecksWritten;
+				break;
 			case '2':
-				cout<<"Creating Savings Account"<<endl;
-				cin.ignore();
-				cin.ignore();
-			break;
 			case '3':
-			   cout<<"Creating High Interest Savings Account"<<endl;
-				cin.ignore();
-				cin.ignore();
-
-			break;
-			case'4':
-				cout<<"Creating Checking Account"<<endl;
-				cin.ignore();
-				cin.ignore();
-
-			break;
-			case'5':
-				cout<<"Creating High Interest Savings Account"<<endl;
-				cin.ignore();
-				cin.ignore();
-
-			break;
-			case'6':
-				cout<<" No service Charge Checking"<<endl;
-				cin.ignore();
-				cin.ignore();
-
-			break;
-			case'7':
-				cout<<" Certificate of Deposit"<<endl;
-				cin.ignore();
-				cin.ignore();
-
-			break;
-			case'8':
-				cout<<"Exit"<<endl;
-				exit = false;
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '0':
+				cout << "Exit" << endl;
+				leaving = true;
 			break;
 			default:
 				cout << "option not available please select a valid option" << endl;
