5cd167ce298311695adf43368375f18ca7a17ff9
zwiesman <zwiesman@gmail.com>
2024-09-18 09:42:34 -0700

    Random num creates a unique account and returns a string now

--- randomnum.cpp
+++ randomnum.cpp
@@ -2,49 +2,37 @@
 
 
 
-int randomNum(){
+string randomNum(){
 
 	int cycles = 0;
 	int randNum = 0;
 	//int numAccountsDifferent;
 	time_t seconds = time(NULL);							// Seed for random numbers, can be used to determine time out in lock function
 	bool exit = false;
+	string filename;
+	string accountNum;
 
 	do{
 
 		srand((unsigned) seconds * (cycles + 27));	// Setting the random number seed
 		randNum = (10000 + rand() % 90000);				// Sets range for random number from 10000 - 99999
-																	// Starts at 10000 in order for the number to always be 5 digits
+		accountNum = to_string(randNum);					// Starts at 10000 in order for the number to always be 5 digits
+		filename = accountNum;
 
-		// Waiting for database, but will just return a random 5 digit number for now
-		/*
-		numAccountsDifferent = 0;							// Checks to see if all the accounts have a different number than generated
+		ifstream filein(filename);
 
-		// Loops through to check if any account number match and counts them, will exit loop if any numbers match
-
-		for(int i = 0; i < numAccounts; i++)
-		{
-			if(randNum != accounts->AcctNumber)
-			{
-				numAccountsDifferent++;
-			}
-			else
-			{
-				i = numAccounts;
-			}
-		}
-
-		// Exit check to see if random number is different than all accounts numbers
-
-		if(numAccountsDifferent == numAccounts)
+		if(!filein)
 		{
 			exit = true;
 		}
-		*/
+
 		cycles++;												// Ensures that the seed will get randomized again while looping
-		exit = true;
 
 	}while(!exit);
 
-	return randNum;
+	cout << accountNum << endl;
+	cin.ignore();
+	cin.ignore();
+
+	return accountNum;										// Returns string instead of int
 }
